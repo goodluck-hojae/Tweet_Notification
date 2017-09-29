69f8e955ceb643a54a784f5ea22b6c6960481eae
hojae <sonhj07>
2017-09-29 11:33:20 +0900

    beta version1.1

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,18 +1,23 @@
 import sys
 import telepot
-
+from googletrans import Translator
 TOKEN = sys.argv[1]  # get token from command-line
 print(TOKEN)
 teleBot = telepot.Bot(TOKEN)
+translator = Translator()
 
-# Telegram Send Message
+# Telegram Send Messagedd
 def sendToTelebot(tweet):
         tele_users = teleBot.getUpdates(offset=100000001)
         tele_userid_set = set()
+        try:
+            for tele_user in tele_users:
+                tele_userid_set.add(tele_user['message']['chat']['id'])
+            print(translator.translate('hello', dest='ko').text)
+            print(translator.translate(tweet, dest='ko').text)
+            tweet = translator.translate(tweet, dest='ko').text+'\n\n'+tweet
+            teleBot.sendMessage(chat_id=-1001125345764, text='%s' % tweet)
+        except telepot.exception.BotWasBlockedError as e:
+            print(e)
+            print('error occured')
 
-        for tele_user in tele_users:
-            tele_userid_set.add(tele_user['message']['chat']['id'])
-        print(tele_userid_set)
-
-        for tele_userid in tele_userid_set:
-            teleBot.sendMessage(tele_userid,tweet)
--- twitter_test.py
+++ twitter_test.py
@@ -1,42 +1,51 @@
-from TwitterSearch import *
-from multiprocessing.pool import ThreadPool
-import _thread
-import time
-import datetime, pytz
 
-tso = TwitterSearchOrder()
-tso.set_keywords(['since:'+str(time.gmtime().tm_year)+'-'+str(time.gmtime().tm_mon)+'-'+str(time.gmtime().tm_mday)], or_operator = True)
-tso.add_keyword("from:dr_coder_kor")
-# it's about time to create a TwitterSearch object with our secret tokens
-ts = TwitterSearch(
-    consumer_key='5z0sfk6FYOa6HQwsW50o2rcTc',
-    consumer_secret='uSf6mPLhqVew9QyWsFUp8N6cUEpBDRNwrU48hRpsMrJzOId7UK',
-    access_token='912715351355383808-ExBFy5wIibRYYuKnjGRarHtzNxXPPnG',
-    access_token_secret='p4VlHmYZwSSUNakbUVz4xU0qrqdX8Vo2EkWXMMu7jkmfR'
-)
+from tweepy import StreamListener
+from tweepy import Stream
+import tweepy
+import TelegramBot
+import json
 
-def retrieveRecentTweet(tso):
-    while 1:
-        for tweet in ts.search_tweets_iterable(tso):
-            tweet_time_info = tweet['created_at'].split(' ')
-            now = datetime.datetime.now()
+consumer_key = '5z0sfk6FYOa6HQwsW50o2rcTc'
+consumer_secret = 'uSf6mPLhqVew9QyWsFUp8N6cUEpBDRNwrU48hRpsMrJzOId7UK'
+access_token_key = '912715351355383808-ExBFy5wIibRYYuKnjGRarHtzNxXPPnG'
+access_token_secret = 'p4VlHmYZwSSUNakbUVz4xU0qrqdX8Vo2EkWXMMu7jkmfR'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_secret)
+api = tweepy.API(auth)
+twitter_name = set()
+class StdOutListener(StreamListener):
 
-            tweet_time = datetime.datetime(int(tweet_time_info[5]),int(time.strptime(tweet_time_info[1],'%b').tm_mon),int(tweet_time_info[2]),
-                                           int(tweet_time_info[3].split(':')[0]),int(tweet_time_info[3].split(':')[1]),int(tweet_time_info[3].split(':')[2]))
+    def on_data(self, data):
+        # process stream data here
+        #TelegramBot.sendToTelebot(data[0]['text'])
+        #TODO REMOVE SPECIAL CHARACTERS
+        tweet = json.loads(data)
+        try:
+            if 'user' in tweet.keys():
+                print('[user][screenname] ' + tweet['user']['screen_name'] )
+                print('check in the twitter list ' + str(tweet['user']['screen_name'].lower() in twitter_name))
+                print(tweet['user']['screen_name'].lower() in twitter_name)
+                print('test')
+                if tweet['user']['screen_name'].lower() in twitter_name :
+                    print('RT in tweet[text] ' + str('RT' in tweet['text']))
+                    print(tweet['text'])
+                    if not 'RT' in tweet['text'][:3] and not '@' in tweet['text'][:3] :
+                        TelegramBot.sendToTelebot(tweet['user']['name'] + '\n' + tweet['text'])
+        except json.decoder.JSONDecodeError:
+            print('json.decoder.JSONDecodeError')
+    def on_error(self, status):
+        print(status)
 
-            #print(now)
-            if abs((tweet_time - now).seconds-15*3600) < 30:
-                print(abs((tweet_time - now).seconds-15*3600))
-                print(tweet['created_at'].split(' '))
-        time.sleep(5)
-           #print('@%s tweeted: %s' % (tweet['user']['screen_name'], tweet['created_at']))
+if __name__ == '__main__':
 
-
-try:
-    _thread.start_new_thread(retrieveRecentTweet,(tso,))
-     # this is where the fun actually starts :)
-
-    while 1:
-        time.sleep(10)
-except TwitterSearchException as e:  # catch all those ugly errors
-    print(e)
+    file = open('twitter_id')
+    list = []
+    list.append('912715351355383808')
+    twitter_name.add('dr_coder_kor')
+    for x in file.readlines():
+        twitter, id = x.split()
+        twitter_name.add(twitter.lower())
+        list.append(id)
+    listener = StdOutListener()
+    twitterStream = Stream(auth, listener)
+    twitterStream.filter(follow=list[:186])

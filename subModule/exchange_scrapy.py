138eed459a95c920905ae978132b04a24357d48d
hojae <sonhj07@gmail.com>
2017-10-15 23:52:34 +0900

    bittrex wallet noti added

--- subModule/exchange_scrapy.py
+++ subModule/exchange_scrapy.py
@@ -6,18 +6,20 @@ import time
 import telepot
 import codecs
 from googletrans import Translator
+import json
 # scrapy genspider quotes toscrape.com
 
 class BinanceSpider(scrapy.Spider):
     name = 'binance'
-    allowed_domains = ['https://support.binance.com/hc/en-us/sections/115000106672-New-Listings','http://bithumb.cafe/notice']
-    start_urls = ['http://support.binance.com/hc/en-us/sections/115000106672-New-Listings','http://bithumb.cafe/notice']
+    allowed_domains = ['https://support.binance.com/hc/en-us/sections/115000106672-New-Listings','http://bithumb.cafe/notice','http://bittrex.com/api/v2.0/pub/currencies/GetWalletHealth?_=1508076591239']
+    start_urls = ['http://support.binance.com/hc/en-us/sections/115000106672-New-Listings','http://bithumb.cafe/notice','http://bittrex.com/api/v2.0/pub/currencies/GetWalletHealth?_=1508076591239']
     base_url = 'http://support.binance.com'
 
     i = 0
     def parse(self, resposne):
         yield Request(url=self.allowed_domains[0], callback=self.parse_binance, dont_filter=True)
         yield Request(url=self.allowed_domains[1], callback=self.parse_bithumb, dont_filter=True)
+        yield Request(url=self.allowed_domains[2], callback=self.parse_bittrex, dont_filter=True)
 
     def parse_binance(self, response):
         binance_listing_file = open('binance_listing', 'r+')
@@ -35,7 +37,7 @@ class BinanceSpider(scrapy.Spider):
             new_list = {'title': binance_news,
              'url': self.base_url + detail_url}
             yield new_list
-            sendToTelebot(new_list)
+            sendToTelebot(new_list['title'],new_list['url'])
             binance_listing_file.seek(0, 2)
             binance_listing_file.write('\n'+new_list['title']+'#'+new_list['url'])
         # repetition/home/hojae/Documents
@@ -43,6 +45,7 @@ class BinanceSpider(scrapy.Spider):
         self.i += 1
         binance_listing_file.close()
         yield Request(url=response.url, callback=self.parse_binance, dont_filter=True)
+        time.sleep(10)
 
     def parse_bithumb(self, response):
         try:
@@ -59,7 +62,7 @@ class BinanceSpider(scrapy.Spider):
                 new_list = {'title': bithumb_news,
                             'url': detail_url}
                 yield new_list
-                sendToTelebot(new_list)
+                sendToTelebot(new_list['title'],new_list['url'])
                 bithumb_notice_file.seek(0, 2)
                 bithumb_notice_file.write('\n'+new_list['title'] + '#' + new_list['url'])
 
@@ -68,7 +71,25 @@ class BinanceSpider(scrapy.Spider):
             self.i += 1
             bithumb_notice_file.close()
             yield Request(url=response.url, callback=self.parse_bithumb, dont_filter=True)
-            time.sleep(10)
+        except IndexError as e:
+
+            print(e)
+
+    def parse_bittrex(self, response):
+        try:
+            bittrex_notice_file = open('bittrex_wallets', 'r+')
+            wallet_set = set()
+            for coin in bittrex_notice_file.readlines():
+                wallet_set.add(coin.strip('\n'))
+            for coin in json.loads(response.text)['result']:
+                if coin['Currency']['CurrencyLong'] not in wallet_set:
+                    print(str(coin['Currency']['CurrencyLong']) + 'is added !')
+                    sendToTelebot(str(coin['Currency']['CurrencyLong']) + ' is added in bittrex wallet!')
+                    bittrex_notice_file.seek(0, 2)
+                    bittrex_notice_file.write('\n'+coin['Currency']['CurrencyLong'])
+
+            bittrex_notice_file.close()
+            yield Request(url=response.url, callback=self.parse_bittrex, dont_filter=True)
         except IndexError as e:
             print(e)
 
@@ -78,7 +99,7 @@ teleBot = telepot.Bot(TOKEN)
 translator = Translator()
 
 # Telegram Send Messagedd
-def sendToTelebot(result):
+def sendToTelebot(title, url=''):
     tele_users = teleBot.getUpdates(offset=100000001)
     tele_userid_set = set()
 
@@ -86,7 +107,7 @@ def sendToTelebot(result):
         for tele_user in tele_users:
             tele_userid_set.add(tele_user['message']['chat']['id'])
 
-        teleBot.sendMessage(chat_id=-1001147113830, text='%s' % result['title'] + '\n\n' + result['url'])
+        teleBot.sendMessage(chat_id=-1001147113830, text='%s' % title + '\n\n' + url)
         # teleBot.sendMessage(chat_id=436399842, text='%s' % tweet) #test bot
     except telepot.exception.BotWasBlockedError as e:
         print(e)

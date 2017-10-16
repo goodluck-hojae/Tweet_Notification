a5a726f0da8c64992eea789f9967b2f6585fc35e
hojae <sonhj07@gmail.com>
2017-10-16 15:39:12 +0900

    eng version added

--- TelegramBot.py
+++ TelegramBot.py
@@ -6,18 +6,22 @@ from googletrans import Translator
 TOKEN = '347870649:AAF2ZfpdDTjssV7mMwJifC3tIfD_gBDUwuU' #sys.argv[1]  # get token from command-line
 print(TOKEN)
 teleBot = telepot.Bot(TOKEN)
+teleBot_en = telepot.Bot('452986802:AAE_zK5iOluBMTpYfEe1L-C8kSTcKkpRr0I')
 translator = Translator()
 
 # Telegram Send Messagedd
 def sendToTelebot(tweet):
+        '''
         tele_users = teleBot.getUpdates(offset=100000001)
         tele_userid_set = set()
         try:
             for tele_user in tele_users:
                 tele_userid_set.add(tele_user['message']['chat']['id'])
-
+        '''
+        try:
             lang = translator.detect(tweet).lang
 
+            teleBot_en.sendMessage(chat_id=-1001125385910, text='%s' % tweet)
             # en -> ko & ko -> en
             if lang == 'en':
                 print(translator.translate(tweet, dest='ko').text)
